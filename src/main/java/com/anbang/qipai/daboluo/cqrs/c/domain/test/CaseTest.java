cba8966da8035b74f013e7dc51adc58b6a448420
林少聪 < @PC-20180515PRDG>
2019-05-15 15:02:41 +0800

    bugfix

--- src/main/java/com/anbang/qipai/daboluo/cqrs/c/domain/test/CaseTest.java
+++ src/main/java/com/anbang/qipai/daboluo/cqrs/c/domain/test/CaseTest.java
@@ -11,6 +11,7 @@ import com.anbang.qipai.daboluo.cqrs.c.domain.DaboluoChupaiDaoCalculator;
 import com.dml.shisanshui.pai.PukePai;
 import com.dml.shisanshui.pai.PukePaiMian;
 import com.dml.shisanshui.pai.paixing.Dao;
+import com.dml.shisanshui.pai.paixing.Paixing;
 
 public class CaseTest {
 	public static void main(String[] args) {
@@ -29,15 +30,84 @@ public class CaseTest {
 		Map<Integer, PukePai> allShoupai = new HashMap<>();
 		for (int i = 0; i < 13; i++) {
 			PukePai pukePai = allPaiList.remove(0);
+			System.out.println(pukePai.getPaiMian());
 			allShoupai.put(pukePai.getId(), pukePai);
 		}
 		DaboluoChupaiDaoCalculator daboluoChupaiDaoCalculator = new DaboluoChupaiDaoCalculator();
 		daboluoChupaiDaoCalculator.setBihuase(true);
-		daboluoChupaiDaoCalculator.setBx(BianXingWanFa.baibian);
+		daboluoChupaiDaoCalculator.setBx(BianXingWanFa.bubian);
 		long s1 = System.currentTimeMillis();
 		List<Dao> daoList = daboluoChupaiDaoCalculator.generateAllPaixingSolution(allShoupai);
 		long s2 = System.currentTimeMillis();
 		System.out.println("总耗时：" + (s2 - s1) + "ms");
+		List<Dao> wulongCandidates = new ArrayList<>();
+		List<Dao> duiziCandidates = new ArrayList<>();
+		List<Dao> liangduiCandidates = new ArrayList<>();
+		List<Dao> santiaoCandidates = new ArrayList<>();
+		List<Dao> shunziCandidates = new ArrayList<>();
+		List<Dao> tonghuaCandidates = new ArrayList<>();
+		List<Dao> huluCandidates = new ArrayList<>();
+		List<Dao> tiezhiCandidates = new ArrayList<>();
+		List<Dao> tonghuashunCandidates = new ArrayList<>();
+		List<Dao> wumeiCandidates = new ArrayList<>();
+		for (Dao dao : daoList) {
+			List<PukePai> pukePaiList = dao.getPukePaiList();
+			if (pukePaiList.size() != 5) {
+				continue;
+			}
+			Paixing paixing = dao.getPaixing();
+			if (Paixing.wulong.equals(paixing)) {
+				if (wulongCandidates.size() > 10) {
+					continue;
+				}
+				wulongCandidates.add(dao);
+			} else if (Paixing.duizi.equals(paixing)) {
+				if (duiziCandidates.size() > 10) {
+					continue;
+				}
+				duiziCandidates.add(dao);
+			} else if (Paixing.liangdui.equals(paixing)) {
+				if (liangduiCandidates.size() > 10) {
+					continue;
+				}
+				liangduiCandidates.add(dao);
+			} else if (Paixing.santiao.equals(paixing)) {
+				if (santiaoCandidates.size() > 10) {
+					continue;
+				}
+				santiaoCandidates.add(dao);
+			} else if (Paixing.shunzi.equals(paixing)) {
+				if (shunziCandidates.size() > 10) {
+					continue;
+				}
+				shunziCandidates.add(dao);
+			} else if (Paixing.tonghua.equals(paixing)) {
+				if (tonghuaCandidates.size() > 10) {
+					continue;
+				}
+				tonghuaCandidates.add(dao);
+			} else if (Paixing.hulu.equals(paixing)) {
+				if (huluCandidates.size() > 10) {
+					continue;
+				}
+				huluCandidates.add(dao);
+			} else if (Paixing.tiezhi.equals(paixing)) {
+				if (tiezhiCandidates.size() > 10) {
+					continue;
+				}
+				tiezhiCandidates.add(dao);
+			} else if (Paixing.tonghuashun.equals(paixing)) {
+				if (tonghuashunCandidates.size() > 10) {
+					continue;
+				}
+				tonghuashunCandidates.add(dao);
+			} else if (Paixing.wumei.equals(paixing)) {
+				if (wumeiCandidates.size() > 10) {
+					continue;
+				}
+				wumeiCandidates.add(dao);
+			}
+		}
 		System.out.println("总数：" + daoList.size());
 	}
 	// }
---
+++ src/main/java/com/anbang/qipai/daboluo/web/controller/SpringMongodbTest.java
@@ -0,0 +1,20 @@
+package com.anbang.qipai.daboluo.web.controller;
+
+//@RunWith(SpringJUnit4ClassRunner.class) // 表示整合JUnit4进行测试
+//@ContextConfiguration(locations = { "classpath:springmongodb.xml" }) // 加载spring配置文件
+public class SpringMongodbTest {
+
+	// @Autowired
+	// private MongoTemplate mongoTemplate;
+	//
+	// @Test
+	// public void test() {
+	// long startTime = System.currentTimeMillis();
+	// Query query = new
+	// Query(Criteria.where("id").is("5d1e4045-f5f1-43e3-abb7-0391a7d872bc"));
+	// GameLatestPanActionFrameDbo dbo = mongoTemplate.findOne(query,
+	// GameLatestPanActionFrameDbo.class);
+	// long endTime = System.currentTimeMillis();
+	// System.out.println("use:" + (endTime - startTime) + "ms");
+	// }
+}
